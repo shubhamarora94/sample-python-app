33a58e61dd839cebf698a9b3a02a705c4f9a1f62
shubhamarora94 <8717073+shubhamarora94@users.noreply.github.com>
2022-02-07 15:14:29 +0530

    Create install-flyway.sh

---
+++ src/install-flyway.sh
@@ -0,0 +1,4 @@
+wget https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/5.0.7/flyway-commandline-5.0.7-linux-x64.tar.gz
+tar xzf flyway-commandline-5.0.7-linux-x64.tar.gz
+rm flyway-commandline-5.0.7-linux-x64.tar.gz
+chmod +x flyway-5.0.7/flyway
